4bc367ffc38637918cc55accaae85547d7c7b391
TehPers <flamingpers@ymail.com>
2013-06-15 21:53:29 -0700

    And more that decided not to get added

---
+++ coffee_common/com/madpc/coffee/item/ItemCoffee.java
@@ -0,0 +1,11 @@
+package com.madpc.coffee.item;
+
+import net.minecraft.item.ItemFood;
+
+public class ItemCoffee extends ItemFood {
+
+    public ItemCoffee(int id) {
+        super(id, 1, 0.0F, false);
+    }
+    
+}
