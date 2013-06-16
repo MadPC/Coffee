ea848462b1324b666b241acc48510b80c68f1fee
TehPers <flamingpers@ymail.com>
2013-06-15 20:52:21 -0700

    PIES

--- coffee_common/com/madpc/coffee/Coffee.java
+++ coffee_common/com/madpc/coffee/Coffee.java
@@ -4,12 +4,19 @@ import com.madpc.coffee.lib.Reference;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class Coffee {
     
-    @Instance("DS")
+    @Instance("Coffee")
     public static Coffee instance;
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
 }
