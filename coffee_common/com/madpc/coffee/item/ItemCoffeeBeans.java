2393d662851268752bdfce07ab9ef75030dc273f
Daniel Sapelli <daniel.sapelli@gmail.com>
2013-06-22 18:45:39 -0400

    Added the basics for Coffee beans. Doesn't do anything yet

---
+++ coffee_common/com/madpc/coffee/item/ItemCoffeeBeans.java
@@ -0,0 +1,10 @@
+package com.madpc.coffee.item;
+
+public class ItemCoffeeBeans extends ItemCustom {
+
+    public ItemCoffeeBeans(int par1) {
+        super(par1);
+        // TODO Auto-generated constructor stub
+    }
+
+}
